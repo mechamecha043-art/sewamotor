2d9acd8b74873b14da2f0234b6d282d9222808f0
Z User <z@container>
2026-07-23 18:09:49 +0000

    de455a09-3486-4d3e-b88d-8b85b9da18a8

--- src/components/sections/LocationContent.tsx
+++ src/components/sections/LocationContent.tsx
@@ -85,7 +85,7 @@ export default function LocationContent() {
               className="rounded-3xl overflow-hidden border border-border shadow-xl"
             >
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.6666666667!2d106.8456!3d-6.2088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMTInMzEuNyJTIDEwNsKwNTAnNDQuMiJF!5e0!3m2!1sid!2sid!4v1234567890"
+                src="https://maps.google.com/maps?q=Rajawali+Motorcycle+Rental+Jakarta&t=&z=14&ie=UTF8&iwloc=&output=embed"
                 width="100%"
                 height="350"
                 style={{ border: 0 }}
